68130724a438db4917eed06c1fecf0a651da2cec
Alden Shin-Culhane <aldenshinculhane@gmail.com>
2023-04-18 17:37:30 -0400

    Vue app logic

---
+++ vue_final/vue_final/src/components/Forecast.vue
@@ -0,0 +1,18 @@
+<script>
+const API_CALL = `http://api.openweathermap.org/data/2.5/weather?q=London,ON,CA&appid=2453333f72b01226743096c5a345d253`
+export default {
+  created() {
+    // fetch on init
+    this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      this.weather = await (await fetch(API_CALL)).json()
+      this.temp = Math.round((this.weather.main.temp - 273.15) * 100)/100.0
+      this.condition = this.weather[0].description
+      this.myTS = this.weather.dt
+      this.$forceUpdate()
+      console.log(this.temp)
+    }
+  }
+}
